28ea617911bfa4782ce45f93de61aff1ecded714
Shevshelev Lev <shevshelev.lev@gmail.com>
2022-05-27 15:06:58 +0500

    Initial Commit

---
+++ GameLife/GameLifeApp.swift
@@ -0,0 +1,17 @@
+//
+//  GameLifeApp.swift
+//  GameLife
+//
+//  Created by Shevshelev Lev on 27.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct GameLifeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
